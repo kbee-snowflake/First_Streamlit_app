06458fc1bfc3a508544aca1619f1354800ba8894
kbee-snowflake <125057343+kbee-snowflake@users.noreply.github.com>
2023-02-10 09:58:20 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit
 streamlit.title('my parents New Healthy Diner')
 streamlit.header('Breakfact Menu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale,spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free Range Egg')
+streamlit.text('🥣Omega 3 and Blueberry Oatmeal')
+streamlit.text('🥗Kale,spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free Range Egg')
+streamlit.text('🥑🍞Hard-Boiled Free Range Egg')
