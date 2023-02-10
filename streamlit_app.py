93ab4d151367ceca3c25ada11084d02cc2029374
kbee-snowflake <125057343+kbee-snowflake@users.noreply.github.com>
2023-02-10 10:02:02 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 
 import streamlit
+import pandas
+
+
+
+
 streamlit.title('my parents New Healthy Diner')
 streamlit.header('Breakfact Menu')
 streamlit.text('🥣Omega 3 and Blueberry Oatmeal')
@@ -7,3 +12,5 @@ streamlit.text('🥗Kale,spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free Range Egg')
 streamlit.text('🥑🍞Hard-Boiled Free Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
